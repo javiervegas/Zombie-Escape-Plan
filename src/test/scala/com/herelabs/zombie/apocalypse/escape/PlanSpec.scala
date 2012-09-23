d7ee742779a561da785a10b923f21e03ef171dc1
Javier Vegas <javiervegas@yahoo.com>
2012-09-23 01:06:19 -0700

    spec for tiredness factor

--- src/test/scala/com/herelabs/zombie/apocalypse/escape/PlanSpec.scala
+++ src/test/scala/com/herelabs/zombie/apocalypse/escape/PlanSpec.scala
@@ -21,6 +21,13 @@ class PlanSpec extends Specification { def is =
     "And each minute of rest gives a 0 seconds boost"                         ^ aNumber^
     "When they path through the quickest path"                                ^ path^
     "Then it should take them 17 minutes"                                     ^ time^
+                                                                              endp^
+  "Group escaping in 17 minutes with no rest boost complications"             ^ br^
+    "Given the group crossing times are 10, 10, 10, 10 minutes"               ^ group^
+    "And the tiredness factor is 20%"                                          ^ aNumber^
+    "And each minute of rest gives a 0 seconds boost"                         ^ aNumber^
+    "When they path through the quickest path"                                ^ path^
+    "Then it should take them 56 minutes"                                     ^ time^
                                                                               end
   object group extends Given[Seq[Minutes]]("\\D*([\\d\\,\\s]+)\\D*") {            
     def extract(text: String): Seq[Minutes] = extract1(text).split("\\,?\\s").map{ s => Minutes.minutes(s.toInt) }
